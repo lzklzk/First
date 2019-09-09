58f5837c31e0ce917a91906f73ef1965f837b1e6
lzklzk <1185137217@qq.com>
2019-09-09 23:49:52 +0800

    begin to learn python爬虫

---
+++ LZK/zkPython/zte.com.ceshi/testDirector/sundany/test.py
@@ -0,0 +1,20 @@
+import threading
+from time import ctime, sleep
+
+
+def music(func):
+    for i in range(2):
+        print("I was listening to%s. %s" %(func,ctime()))
+        sleep(1)
+
+
+def moive(func):
+    for i in range(2):
+        print("I was at the %s! %s" %(func,ctime()))
+        sleep(5)
+
+
+if __name__ == '__main__':
+    music(u'爱情买卖')
+    moive(u'阿凡达')
+    print("all over %s" % ctime())
---
+++ LZK/zkPython/zte.com.ceshi/testDirector/sundany/tst.py
@@ -0,0 +1,19 @@
+from time import ctime,sleep
+
+
+def music():
+    for i in range(2):
+        print("I was listening to music.%s" %  ctime())
+        sleep(1)
+
+
+def movie():
+    for i in range(2):
+        print("I was at the movies! %s" % ctime())
+        sleep(5)
+
+
+if __name__ == '__main__':
+    music()
+    movie()
+    print("all over %s" % ctime())
